4aa44cf55a0a990566ad80ebea7809f81bcfc0d1
romain234 <so.romain77@gmail.com>
2025-03-22 17:50:56 +0100

    Fix image paths for GitHub Pages

--- src/app/mock-competences.ts
+++ src/app/mock-competences.ts
@@ -3,25 +3,25 @@ import { Competences } from './competence';
 export const COMPETENCES: Competences[] = [
     {
         domaine: "3D",
-        domaineImg:"/assets/img/icone_3D.png",
+        domaineImg:"assets/img/icone_3D.png",
         description: "projet 3D",
         technologie: ["Blender", " Unity"],
     },
     {
         domaine: "Dev",
-        domaineImg:"/assets/img/icone_dev.png",
+        domaineImg:"assets/img/icone_dev.png",
         description: "projet dev",
         technologie: ["Blender", " Unity"],
     },
     {
         domaine: "Montage",
-        domaineImg:"/assets/img/icone_montage.png",
+        domaineImg:"assets/img/icone_montage.png",
         description: "projet Montage",
         technologie: ["Blender", " Unity"],
     },
     {
         domaine: "Graphisme",
-        domaineImg:"/assets/img/icone_design.png",
+        domaineImg:"assets/img/icone_design.png",
         description: "projet Design",
         technologie: ["Blender", " Unity"],
     },
